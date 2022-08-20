cc279654813f5b0de1c136bbc0b9b3956709b944
YhomiAce <kareemyomi91@gmail.com>
2022-08-20 14:05:27 +0100

    changes

--- service/emailService.js
+++ service/emailService.js
@@ -4,11 +4,18 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false, // true for 465, false for other ports
+    secure: process.env.EMAIL_SECURE, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USERNAME, // generated ethereal user
         pass: process.env.EMAIL_PASSWORD, // generated ethereal password
-    }
+    },
+    tls: {
+        rejectUnauthorized: false
+    },
+    ool: true,
+    maxConnections: 1,
+    rateDelta: 20000,
+    rateLimit: 5,
 });
 
 exports.sendMail = async (email, message, subject) => {
